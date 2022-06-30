b437be91a7544a52825cf06064681ca4a3e4ece3
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-06-30 21:30:11 +0700

    fix: add error handling and validation

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,7 +2,7 @@ const profile = require('express').Router();
 const {body} = require('express-validator');
 const profileControler = require('../controllers/profile');
 const validatorCreate = [
-  body('num_phone').isNumeric().withMessage('Number Not Valid')]
+  body('num_phone').isNumeric().withMessage('Number Not Valid')];
 
 
 profile.get('/',profileControler.getListProfile);
