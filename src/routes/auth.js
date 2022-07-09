0e330432042136410e328e01fed559e89fcf2308
ramdhanstdi <ramdhansetiadhi@gmail.com>
2022-07-09 17:08:58 +0700

    feat: add router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,17 @@
 const auth = require('express').Router();
-const {rulesUsers, rulesPin} = require('./validator');
+const {rulesUsers, rulesPin, rulesProfile} = require('./validator');
 const validation = require('../middleware/validation');
 const controlAuth = require('../controllers/auth');
+const authorization = require('../middleware/auth');
+const uploadPhoto = require('../middleware/upload');
 
 auth.post('/register',...rulesUsers,validation,controlAuth.register);
 auth.post('/createPin',...rulesPin,validation,controlAuth.createPin);
 auth.post('/login',controlAuth.login);
+
+
+//User was Login
+auth.get('/profile',authorization,controlAuth.getProfile);
+auth.patch('/profile',authorization,uploadPhoto,...rulesProfile,validation,controlAuth.updateProfile);
 
 module.exports = auth;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,5 @@
+const auth = require('../middleware/auth');
+
 const router = require('express').Router();
 
 //Router Admin
@@ -5,7 +7,17 @@ router.use('/admin/users', require('./users'));
 router.use('/admin/transaction', require('./transaction'));
 router.use('/admin/profile', require('./profile'));
 router.use('/admin/typeTransaction',require('./typeTransaction'));
+router.use('/authentication',auth,(req,res)=>{
+  const userModels = require('../models/users');
+  userModels.getDetailUser(req.userAuth.id,(err,result)=>{
+    const user = result.rows[0];
+    return res.json({
+      massages: 'Hallo'+user.username
+    });
+  });
+});
 
 //Router Users
-router.use('/auth', require('./auth'));
+router.use('/',require('./auth'));
+router.use('/auth',require('./auth'));
 module.exports = router;
